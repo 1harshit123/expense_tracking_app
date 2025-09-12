17544824d60c87c0e301f3878cd4d7d24cb70090
1harshit123 <harshitkumar100436@gmail.com>
2025-09-13 03:08:24 +0530

    authentication complete

--- backend/trackingBackend/myAPI/views.py
+++ backend/trackingBackend/myAPI/views.py
@@ -2,11 +2,38 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import UserProfile, Expense
-from .serializers import UserProfileSerializer, ExpenseSerializer
+from .serializers import UserProfileSerializer, ExpenseSerializer, RegistrationSerializer, LoginSerializer
+from django.contrib.auth import login
 
+api_view('POST')
 def user_registration(request):
     if request.method == 'post':
-        serializer = 
+        serializer = RegistrationSerializer(data = request.data)
+        if serializer.is_valid():
+            UserProfile.objects.create_user(
+                serializer.initial_data('username'),
+                serializer.initial_data('email'),
+                serializer.initial_data('password')
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+api_view('POST')
+def user_login(request):
+    if request.method == 'post':
+        serializer = LoginSerializer(data = request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            login(request, user)
+            details = UserProfile(user).data
+            return Response(
+            {"message": "Login successful", "user": details},
+            status=status.HTTP_200_OK
+        )
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 def getting_daily_expenses(request):
